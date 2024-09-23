a0d0fa030f178a18d2649a84d547f534e9f0978d
osamaelsaied <osamaelsaeed28@gmail.com>
2024-09-23 12:21:06 +0300

    Resiliance4J circuit breaker ratelimiter fallback method and retry

--- job-ms/job-ms/src/main/java/com/job_ms/job_ms/job/jobServiceImpl/JobServiceImpl.java
+++ job-ms/job-ms/src/main/java/com/job_ms/job_ms/job/jobServiceImpl/JobServiceImpl.java
@@ -10,6 +10,9 @@ import com.job_ms.job_ms.job.dto.JobDTO;
 import com.job_ms.job_ms.job.external.Company;
 import com.job_ms.job_ms.job.external.Review;
 import com.job_ms.job_ms.job.mapper.JobMapper;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
@@ -17,6 +20,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -35,6 +39,8 @@ public class JobServiceImpl implements JobService {
     @Autowired
     private ReviewClient reviewClient;
 
+    int attempt = 0;
+
 
     @Override
     public Job addJob(Job job) {
@@ -43,10 +49,20 @@ public class JobServiceImpl implements JobService {
     }
 
     @Override
+    //@CircuitBreaker(name = "companyBreaker", fallbackMethod = "companyBreakerFallback")
+    //@Retry(name = "companyBreaker", fallbackMethod = "companyBreakerFallback")
+    @RateLimiter(name = "companyBreaker", fallbackMethod = "companyBreakerFallback")
     public List<JobDTO> findAllJobs() {
+        System.out.println("At  tempt: "+ ++attempt);
         List<Job> jobs = jobRepository.findAll();
         return jobs.stream().map(this::convertToDTO).collect(Collectors.toList());
 
+    }
+
+    public List<String> companyBreakerFallback(Exception e){
+        List<String> list = new ArrayList<>();
+        list.add("Dummy");
+        return list;
     }
 
     private JobDTO convertToDTO(Job job){
